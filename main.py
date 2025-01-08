a08b73591eafdabfc96dbb1b3a054977b189b90a
Silent-Suspect <Sascha.Mueller@rub.de>
2025-01-08 18:09:48 +0100

    init pygame loop, create main window

--- main.py
+++ main.py
@@ -2,6 +2,27 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    loop = 1
+    while loop:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = 0
+        pygame.display.flip()  # display update
+    
+    pygame.quit()
+
+
 
 
 if __name__ == "__main__":
